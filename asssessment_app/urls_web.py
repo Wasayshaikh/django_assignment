8939d3389c2faee7807c7df87784682f87a41188
Wasayshaikh <abdulwasayshaikh1999@gmail.com>
2025-07-17 18:26:21 +0800

    add transaction url and fix method name

--- asssessment_app/urls_web.py
+++ asssessment_app/urls_web.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index,upload_invoice_view, reconciliation_log_view,match_log_view,unmatched_log_view,manual_match_transaction
+from .views import index, bank_transaction_view, invoice_view, reconciliation_log_view,match_log_view,unmatched_log_view,manual_match_transaction
 
 urlpatterns = [
     path('', index, name='home'),
-    path('upload-invoice', upload_invoice_view, name='upload_invoice'),
+    path('upload-transaction', bank_transaction_view, name='home'),
+    path('upload-invoice', invoice_view, name='upload_invoice'),
     path('reconciliation-log', reconciliation_log_view, name='logs'),
     path('matched-log', match_log_view, name='match_logs'),
     path('unmatched-log', unmatched_log_view, name='unmatched_logs'),
